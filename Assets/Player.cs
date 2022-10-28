724b7ef1aae7520e1228689b1e2bc26ca3520e33
MrRighto <mrrighto.999@gmail.com>
2022-10-28 09:12:05 -0500

    First Proper Commit

---
+++ Assets/Player.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print("hello there");
+        transform.Translate(0, 1, 0);
+    }
+}
