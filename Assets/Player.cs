8ac47b79a480bd8fc172461eec4a3e463a01d7d2
MrRighto <mrrighto.999@gmail.com>
2022-10-29 18:03:41 -0500

    Grid Based Movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public int speed = 50;
+    private bool isMoving;
+    private Vector3 origPos, targetPos;
+    private float timeToMove = 0.2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +18,41 @@ public class Player : MonoBehaviour
     {
         //print("hello there");
         //transform.Translate(0, 1, 0);
-        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, Input.GetAxis("Vertical") * Time.deltaTime * speed, 0f);
+        //transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, Input.GetAxis("Vertical") * Time.deltaTime * speed, 0f);
+        
+        if (Input.GetKey(KeyCode.W) && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.up));
+
+        if (Input.GetKey(KeyCode.A) && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.left));
+
+        if (Input.GetKey(KeyCode.S) && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.down));
+
+        if (Input.GetKey(KeyCode.D) && !isMoving)
+            StartCoroutine(MovePlayer(Vector3.right));
+
+    }
+
+    private IEnumerator MovePlayer(Vector3 direction)
+    {
+        isMoving = true;
+
+        float elapsedTime = 0;
+
+        origPos = transform.position;
+        targetPos = origPos + (direction * 8);
+
+
+        while(elapsedTime < timeToMove)
+        {
+            transform.position = Vector3.Lerp(origPos, targetPos, (elapsedTime / timeToMove));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPos;
+
+        isMoving = false;
     }
 }
